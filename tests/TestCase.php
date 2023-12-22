dfd79fb9b8e4117c100efdbb862b5907675113d2
fuminopen <fuminopen@gmail.com>
2023-12-22 14:48:16 +0900

    enable bypass finals

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,17 @@
 
 namespace Tests;
 
+use DG\BypassFinals;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        BypassFinals::enable();
+    }
 }
